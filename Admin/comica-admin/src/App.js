891602d8062f2f06975c74450dbf7041174f49b6
Kiran Kamble <kirankamble4595@gmail.com>
2022-11-10 21:24:29 +0530

    auth&user done

--- Admin/comica-admin/src/App.js
+++ Admin/comica-admin/src/App.js
@@ -1,9 +1,15 @@
 import * as React from "react";
-import { Admin } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import { Admin, ListGuesser, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+import authProvider from "./AuthProvider/authProvider";
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
-const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (
+  <Admin dataProvider={dataProvider} authProvider={authProvider}  >
+    <Resource name="users" list={ListGuesser} />
+    <Resource name="posts" list={ListGuesser} />
+  </Admin>
+);
 
 export default App;
