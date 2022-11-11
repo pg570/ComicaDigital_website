675861412842d64d33e5c498111e14ca71e04a32
unknown <priyarajawat>
2022-11-11 07:50:21 +0530

    payment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Detail from './components/ProductDetail/Detail';
+import Paymets from './pages/Payments Page/Paymets';
 
 function App() {
   return (
     <div className="App">
       Home
+      <Detail/>
+      <Paymets/>
     </div>
   );
 }
