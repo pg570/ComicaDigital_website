f2108744b042a28dd86916f03b6f81dcab847957
madhusainadh <lalammadhusainadh150@gmail.com>
2022-12-27 11:01:37 +0530

    fixed bugs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter  } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
@@ -12,11 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store} >
     <ChakraProvider>
-      <BrowserRouter>
+      <HashRouter >
      
         <App />
    
-      </BrowserRouter>
+      </HashRouter>
     </ChakraProvider>
   </Provider>
 );
